a49fc74ed3cada9d8a0d1e531f5555e623214e62
Zorca <vs@zorca.org>
2017-04-15 14:14:12 +0300

    Complete Laravel Mix implementation

---
+++ webpack.mix.js
@@ -0,0 +1,18 @@
+const { mix } = require('laravel-mix');
+const config = require('./resources/assets/config');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/assets/scripts/main.js', 'dist/scripts')
+   .sass('resources/assets/styles/main.scss', 'dist/styles');
+
+mix.browserSync(config.devUrl);
